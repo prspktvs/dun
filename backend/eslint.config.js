6472339cfbfe9f835136435c8e23d4295389398c
Eugene Ward <razzy.sa@gmail.com>
2024-11-15 01:35:07 +0100

    fixes

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -14,6 +14,7 @@ export default [
     rules: {
       'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
       'no-case-declarations': 'off',
+      semi: ['error', 'never'],
     },
   },
 ]
