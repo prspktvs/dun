ea823b45844f07db59eb9dc89002a3287d5a43bb
alex <againullin8@gmail.com>
2025-01-29 13:13:35 +0200

    sharing4

--- app/src/pages/Project/CardTabs.tsx
+++ app/src/pages/Project/CardTabs.tsx
@@ -28,52 +28,50 @@ const CardTabs = ({
   if (!isMobile) {
     return (
       <div className='flex items-center justify-between h-14 border-b-1 border-borders-purple'>
-        <div className='w-full overflow-x-auto hide-scrollbar'>
-          <div className='inline-flex h-full divide-x-[1px] divide-borders-gray border-borders-purple'>
-            <div
-              className={clsx(
-                'flex items-center justify-center font-monaspace text-14 lg:text-sm whitespace-nowrap w-[120px]',
-                activeTab === 'discussions'
-                  ? 'bg-background text-black'
-                  : 'bg-grayBg text-inactiveText',
-              )}
-              onClick={() => setActiveTab('discussions')}
-            >
-              Discussions• {cardChatsLength}
-              <UnreadIndicator
-                size='sm'
-                count={unreadDiscussions}
-                className='relative -top-2 left-1'
-              />
-            </div>
-            <div
-              className={clsx(
-                'flex items-center justify-center font-monaspace text-14 lg:text-sm whitespace-nowrap w-[120px]',
-                activeTab === 'attachments'
-                  ? 'bg-background text-black'
-                  : 'bg-grayBg text-inactiveText',
-              )}
-              onClick={() => setActiveTab('attachments')}
-            >
-              Attachments• {filesLength}
-            </div>
-            {isAuthor && (
-              <div
-                className={clsx(
-                  'flex items-center justify-center font-monaspace text-14 lg:text-sm whitespace-nowrap w-[120px]',
-                  activeTab === 'sharing'
-                    ? 'bg-background text-black'
-                    : 'bg-[#edebf3] text-inactiveText',
-                )}
-                onClick={() => {
-                  setActiveTab('sharing')
-                  openShareModal()
-                }}
-              >
-                Sharing
-              </div>
+        <div className='w-full grid grid-cols-3 h-full divide-x-[1px] divide-borders-gray border-borders-purple'>
+          <div
+            className={clsx(
+              'flex items-center justify-center font-monaspace text-14 lg:text-sm',
+              activeTab === 'discussions'
+                ? 'bg-background text-black'
+                : 'bg-grayBg text-inactiveText',
             )}
+            onClick={() => setActiveTab('discussions')}
+          >
+            Discussions• {cardChatsLength}
+            <UnreadIndicator
+              size='sm'
+              count={unreadDiscussions}
+              className='relative -top-2 left-1'
+            />
           </div>
+          <div
+            className={clsx(
+              'flex items-center justify-center font-monaspace text-14 lg:text-sm',
+              activeTab === 'attachments'
+                ? 'bg-background text-black'
+                : 'bg-grayBg text-inactiveText',
+            )}
+            onClick={() => setActiveTab('attachments')}
+          >
+            Attachments• {filesLength}
+          </div>
+          {isAuthor && (
+            <div
+              className={clsx(
+                'flex items-center justify-center font-monaspace text-14 lg:text-sm',
+                activeTab === 'sharing'
+                  ? 'bg-background text-black'
+                  : 'bg-grayBg text-inactiveText',
+              )}
+              onClick={() => {
+                setActiveTab('sharing')
+                openShareModal()
+              }}
+            >
+              Sharing Settings
+            </div>
+          )}
         </div>
       </div>
     )
@@ -85,7 +83,7 @@ const CardTabs = ({
         <div className='inline-flex h-full divide-x-[1px] divide-borders-gray border-borders-purple'>
           <div
             className={clsx(
-              'flex items-center justify-center font-monaspace text-sm px-3 py-2 lg:text-sm whitespace-nowrap',
+              'flex items-center justify-center font-monaspace text-sm px-3 py-2 lg:text-sm whitespace-nowrap w-[120px]',
               activeTab === 'editor'
                 ? 'bg-background text-black'
                 : 'bg-[#edebf3] text-inactiveText',
