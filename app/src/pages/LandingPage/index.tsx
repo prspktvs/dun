cac3b9f133ad203c1a80e3e935be044862ed4069
Eugene Ward <razzy.sa@gmail.com>
2025-03-24 12:48:51 +0100

    adds to landing

--- app/src/pages/LandingPage/index.tsx
+++ app/src/pages/LandingPage/index.tsx
@@ -18,41 +18,44 @@ const NAVIGATION_ITEMS = [
 export const LandingHeader = () => {
   const { user } = useAuth()
   return (
-    <header className='w-full h-14 overflow-hidden flex flex-row items-start justify-center gap-px border-b border-black bg-[#c5d4d2]'>
-      {/* Logo */}
-      <a
-        href='/'
-        className='grow shrink basis-0 md:grow-0 self-stretch flex flex-col items-start md:items-center justify-center px-2.5 md:px-5 lg:px-10 border-r-[1px] border-black no-underline'
-      >
-        <Logo />
-      </a>
+    <div className='fixed top-0 left-2 right-2 z-50'>
+      <div className='h-2 bg-[#faf9f6]' />
+      <header className='border-t-1 border-l-1 border-r-1 h-14 overflow-hidden flex flex-row items-start justify-center gap-px border-b border-black bg-[#c5d4d2] z-40'>
+        {/* Logo */}
+        <a
+          href='/'
+          className='grow shrink basis-0 md:grow-0 self-stretch flex flex-col items-start md:items-center justify-center px-2.5 md:px-5 lg:px-10 border-r-[1px] border-black no-underline'
+        >
+          <Logo />
+        </a>
 
-      {/* Navigation */}
-      <nav className='items-center self-stretch justify-center flex-1 hidden gap-2 px-2 md:flex md:gap-10 md:px-10'>
-        {NAVIGATION_ITEMS.map((item) => (
+        {/* Navigation */}
+        <nav className='items-center self-stretch justify-center flex-1 hidden gap-2 px-2 md:flex md:gap-10 md:px-10'>
+          {NAVIGATION_ITEMS.map((item) => (
+            <a
+              key={item.id}
+              href={`/#${item.id}`}
+              className='flex items-center justify-center pt-3 pb-2 no-underline'
+            >
+              <span className='text-[#343434] hover:opacity-75 text-base font-medium font-monaspace uppercase whitespace-nowrap'>
+                {item.name}
+              </span>
+            </a>
+          ))}
+        </nav>
+
+        <div className='hidden md:flex flex-col items-center self-stretch justify-center border-l border-black px-2.5 md:px-5 lg:px-10'>
           <a
-            key={item.id}
-            href={`/#${item.id}`}
-            className='flex items-center justify-center pt-3 pb-2 no-underline'
+            href={user ? '/dashboard' : '/login'}
+            className='h-[50px] py-2 flex items-center justify-center no-underline'
           >
-            <span className='text-[#343434] hover:opacity-75 text-base font-medium font-monaspace uppercase whitespace-nowrap'>
-              {item.name}
+            <span className='text-[#343434] hover:opacity-75 text-base font-semibold font-monaspace  whitespace-nowrap'>
+              {user ? 'Go to Dashboard' : 'Try for free'}
             </span>
           </a>
-        ))}
-      </nav>
-
-      <div className='hidden md:flex flex-col items-center self-stretch justify-center border-l border-black px-2.5 md:px-5 lg:px-10'>
-        <a
-          href={user ? '/dashboard' : '/login'}
-          className='h-[50px] py-2 flex items-center justify-center no-underline'
-        >
-          <span className='text-[#343434] hover:opacity-75 text-base font-semibold font-monaspace  whitespace-nowrap'>
-            {user ? 'Go to Dashboard' : 'Try for free'}
-          </span>
-        </a>
-      </div>
-    </header>
+        </div>
+      </header>
+    </div>
   )
 }
 
