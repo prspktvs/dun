ca51b9d6114fca8b16bf3766881bacc6c75b9572
Eugene Ward <razzy.sa@gmail.com>
2025-04-07 01:23:40 +0200

    remove vite-plugin-pwa

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,41 +1,10 @@
 import { defineConfig, loadEnv } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
-    plugins: [
-      react(),
-      VitePWA({
-        injectRegister: false,
-        injectManifest: false,
-        workbox: {
-          maximumFileSizeToCacheInBytes: 4 * 1024 * 1024,
-        },
-        manifest: {
-          name: 'Dun App',
-          short_name: 'Dun',
-          description: 'Solve your 50+ open tabs problem',
-          start_url: '/',
-          display: 'standalone',
-          background_color: '#ffffff',
-          theme_color: '#000000',
-          icons: [
-            {
-              src: '/icons/icon-192x192.png',
-              sizes: '192x192',
-              type: 'image/png',
-            },
-            {
-              src: '/icons/icon-512x512.png',
-              sizes: '512x512',
-              type: 'image/png',
-            },
-          ],
-        },
-      }),
-    ],
+    plugins: [react()],
     define: {
       'process.env': env,
     },
