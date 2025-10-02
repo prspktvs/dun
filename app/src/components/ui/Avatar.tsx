a2ac00dffdf09f6cd222badc0f056afa865e915b
eugeek <razzy.sa@gmail.com>
2025-10-02 12:48:10 +0200

    fix avatar

--- app/src/components/ui/Avatar.tsx
+++ app/src/components/ui/Avatar.tsx
@@ -8,20 +8,43 @@ type UserType = IUser | firebase.User
 
 const AvatarDun = React.forwardRef<HTMLDivElement, { user: UserType; size?: AvatarProps['size'] }>(
   ({ user, size = 36 }, ref) => {
-    const name = user ? ('name' in user ? user.name : user.displayName)?.split(' ') || [] : []
+    if (!user) {
+      return (
+        <Avatar
+          ref={ref}
+          size={size}
+          color='blue'
+          radius={3}
+          style={{ border: '1.5px solid black' }}
+        >
+          <span className='pt-1 font-monaspace text-black'>U</span>
+        </Avatar>
+      )
+    }
+
+    const name = ('name' in user ? user.name : user.displayName) || ''
+    const nameParts = name.split(' ')
     const initials =
-      name.length > 1 ? name[0].charAt(0) + name[1]?.charAt(0) : name[0]?.charAt(0) || 'U'
+      nameParts.length > 1
+        ? `${nameParts[0][0] ?? ''}${nameParts[1][0] ?? ''}`
+        : (nameParts[0][0] ?? 'U')
+
     const avatarUrl =
-      'avatarUrl' in user ? user.avatarUrl : ((user as firebase.User).photoURL as string)
-    const color = 'color' in user ? user.color : 'blue'
-    const displayName = 'name' in user ? user.name : user.displayName
+      'avatarUrl' in user && typeof user.avatarUrl === 'string' && user.avatarUrl
+        ? user.avatarUrl
+        : 'photoURL' in user && typeof user.photoURL === 'string' && user.photoURL
+          ? user.photoURL
+          : null
+
+    const color = 'color' in user && user.color ? user.color : 'blue'
+    const displayName = name || 'User'
 
     return (
       <Avatar
         ref={ref}
         size={size}
         src={avatarUrl}
-        alt={displayName || undefined}
+        alt={displayName}
         color={color}
         radius={3}
         style={{ border: '1.5px solid black' }}
